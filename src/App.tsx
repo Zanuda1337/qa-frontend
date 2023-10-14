166d000a7d368b335947e1771eab1a1274360880
Zanuda1337 <dsnake456@gmail.com>
2023-10-14 11:01:03 +0300

    changed favicon & preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import 'src/assets/styles/reset.css';
 import 'src/assets/styles/global.scss';
 import Router from 'src/router/Router';
 import { initialize } from 'src/App.slice';
 import {useAppSelector, useBoundActions} from 'src/app/hooks';
+import Preloader from "src/components/Preloader/Preloader";
 
 const App: React.FC = () => {
   const initialized = useAppSelector((state) => state.appReducer.initialized);
   const boundActions = useBoundActions({initialize})
+  const [initializedFlag, setInitializedFlag] = useState(false);
+
+  useEffect(() => {
+    if(!initialized) return;
+    setTimeout(() => setInitializedFlag(true), 500);
+  }, [initialized])
+
   useEffect(() => {
     boundActions.initialize();
   }, []);
-  if (!initialized)
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: '10rem',
-	        height: '100vh'
-        }}
-      >
-        LOADING...
-      </div>
-    );
-  return <Router />;
+
+  return <>
+    {!initializedFlag && <Preloader visible={!initialized}/>}
+    {initialized && <Router/>}
+    </>;
 };
 
 export default App;
